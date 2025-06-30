d99005e2a7de818ecf6d669a57d1397093a0fd9f
VadimContentHunter <project.k.vadim@gmail.com>
2025-06-30 11:43:51 +0300

    feat(delete-user): add executeByObject method to DeleteUser use case

--- public/app/src/components/UserManagement/DeleteUser.php
+++ public/app/src/components/UserManagement/DeleteUser.php
@@ -2,15 +2,18 @@
 
 namespace crm\src\components\UserManagement;
 
-use crm\src\components\UserManagement\common\interfaces\IUserRepository;
-use crm\src\components\UserManagement\common\interfaces\IUserResult;
+use crm\src\components\UserManagement\common\DTOs\UserDto;
 use crm\src\components\UserManagement\common\adapters\UserResult;
+use crm\src\components\UserManagement\common\interfaces\IUserResult;
+use crm\src\components\UserManagement\common\interfaces\IUserRepository;
+use crm\src\components\UserManagement\common\interfaces\IUserValidation;
 use crm\src\components\UserManagement\common\exceptions\UserManagementException;
 
 class DeleteUser
 {
     public function __construct(
         private IUserRepository $userRepository,
+        private IUserValidation $validator,
     ) {
     }
 
@@ -55,6 +58,26 @@ class DeleteUser
             }
 
             return UserResult::success($deletedId);
+        } catch (\Throwable $e) {
+            return UserResult::failure($e);
+        }
+    }
+
+    public function executeByObject(UserDto $dto): IUserResult
+    {
+        $validationResult = $this->validator->validate($dto);
+
+        if (!$validationResult->isValid()) {
+            return UserResult::failure(
+                new UserManagementException('Ошибка валидации: ' . implode('; ', $validationResult->getErrors()))
+            );
+        }
+
+        try {
+            // Предполагаем, что валидатор гарантирует наличие одного из полей
+            return $dto->id > 0
+            ? $this->executeById($dto->id)
+            : $this->executeByLogin($dto->login);
         } catch (\Throwable $e) {
             return UserResult::failure($e);
         }
